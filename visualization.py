4682538c6a2b014ab00c0a5e02d42e9573600be7
Koichi <ss-g1@ezweb.ne.jp>
2021-06-09 20:43:51 +0900

    chang change

--- visualization.py
+++ visualization.py
@@ -85,15 +85,17 @@ if __name__ == '__main__':
     pre_img = change(pre_img)
 
     #matplotlibで変更前と変更後の画像の可視化
-    #fig1 = plt.figure()
+    #fig1 = plt.figure(figsize=(640,480))
     #ax1 = fig1.add_subplot(111)
     #ax1.set_title('before preprocessing')
-    #img1 = ax1.imshow(before_img)
+    #mg1 = ax1.imshow(before_img)
 
-    #fig2 = plt.figure()
+    #fig2 = plt.figure(figsize=(640,480))
     #ax2 = fig2.add_subplot(111)
     #ax2.set_title('after prerprocessing')
     #img2 = ax2.imshow(pre_img)
+    
+    
     plt.subplot(121).imshow(before_img)
     plt.subplot(122).imshow(pre_img)
     plt.show()
